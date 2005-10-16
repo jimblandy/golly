e082e93f0a92814868c3bfb638463152e8436792
rokicki <rokicki>
2005-10-16 00:44:24 +0000

    Made it work on one big line so we can make changes that include end of line markers.

--- Help/Lexicon/modify.pl
+++ Help/Lexicon/modify.pl
@@ -4,31 +4,32 @@ for $file (@files) {
    open F, "$file" or die "Can't open $file\n" ;
    @f = <F> ;
    close F ;
-   for (@f) {
-      s/<body>/<body bgcolor="#FFFFCE"><font size=-1>/ ;
-      s!<center><a HREF="http://www.argentum.freeserve.co.uk/lex_home.htm">Life Lexicon Home Page</a>!<center><A HREF="lex.htm">Introduction</A> \| <A HREF="lex_bib.htm">Bibliography</A></center>! ;
-      s|<center><b class=l>Life Lexicon</b></center>|| ;
-      s:^<A HREF="lex.htm">Introduction</A> \|:<font size=-1><b>: ;
-      s:^<a href="lex.htm">Introduction</a> \|:<font size=-1><b>: ;
-      s:<A HREF="lex_z.htm">Z</A> \|:<A href="lex_z.htm">Z</A></b></font>: ;
-      s:<a href="lex_z.htm">Z</a> \|:<A href="lex_z.htm">Z</A></b></font>: ;
-      s|^<A HREF="lex_bib.htm">Bibliography</A>|| ;
-      s|^<a href="lex_bib.htm">Bibliography</a>|| ;
-      s|<h3|<h5| ; s|</h3|</h5| ;
-      s|<H3|<h5| ; s|</H3|</h5| ;
-      s/^\t*// ;
+   $f = join('', @f) ;
+   for (($f)) {
+      s/<body>/<body bgcolor="#FFFFCE"><font size=-1>/g ;
+      s!<center><a HREF="http://www.argentum.freeserve.co.uk/lex_home.htm">Life Lexicon Home Page</a>!<center><A HREF="lex.htm">Introduction</A> \| <A HREF="lex_bib.htm">Bibliography</A></center>!g ;
+      s|<center><b class=l>Life Lexicon</b></center>||g ;
+      s:^<A HREF="lex.htm">Introduction</A> \|:<font size=-1><b>:gm ;
+      s:^<a href="lex.htm">Introduction</a> \|:<font size=-1><b>:gm ;
+      s:<A HREF="lex_z.htm">Z</A> \|:<A href="lex_z.htm">Z</A></b></font>:g ;
+      s:<a href="lex_z.htm">Z</a> \|:<A href="lex_z.htm">Z</A></b></font>:g ;
+      s|^<A HREF="lex_bib.htm">Bibliography</A>||gm ;
+      s|^<a href="lex_bib.htm">Bibliography</a>||gm ;
+      s|<h3|<h5|g ; s|</h3|</h5|g ;
+      s|<H3|<h5|g ; s|</H3|</h5|g ;
+      s/^\t*//gm ;
       s/&times;/x/g ;
       s/&oacute;/o/g ;
-      s|^<pre>|<center><table cellspacing=0 cellpadding=0><tr><td><pre><a href="lexpatt:">| ;
-      s|^</pre>|</a></pre></td></tr></table></center>| ;
-      s|^</BODY>|</font></body>| ;
-      s|^</body>|</font></body>| ;
+      s|^<pre>|<center><table cellspacing=0 cellpadding=0><tr><td><pre><a href="lexpatt:">|gm ;
+      s|^</pre>|</a></pre></td></tr></table></center>|gm ;
+      s|^</BODY>|</font></body>|gm ;
+      s|^</body>|</font></body>|gm ;
       # fix problems in lex_[hps].htm
-      s|<a href="lexpatt:">\n-|<font size=-1>\n-| ; s|-\n</a>|-\n</font>| ;
+      s|<a href="lexpatt:">\n-|<font size=-1>\n-|g ; s|-\n</a>|-\n</font>|g ;
       # fix problem in lex_l.htm
-      s|<a href="lexpatt:">\nMar|<font size=-1>\nMar| ; s|1973\n</a>|1973\n</font>| ;
+      s|<a href="lexpatt:">\nMar|<font size=-1>\nMar|g ; s|1973\n</a>|1973\n</font>|g ;
    }
    open F, ">$file" or die "Can't write $file\n" ;
-   print F @f ;
+   print F $f ;
    close F ;
 }
