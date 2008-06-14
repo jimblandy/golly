b91c28282447637678fec0a0e04b194432a28855
rokicki <rokicki>
2008-06-14 21:43:48 +0000

    Fixed some compilation issues that showed up on linux but not Windows.

--- generationsalgo.cpp
+++ generationsalgo.cpp
@@ -22,6 +22,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
                         / ***/
 #include "generationsalgo.h"
+#include <string.h>
+using namespace std ;
 
 int generationsalgo::NumCellStates() {
    return maxCellStates ;
