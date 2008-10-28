9a2988193f635c0b98cfdc3bed75ce427672d226
tim_hutton <tim_hutton>
2008-10-28 23:10:40 +0000

    Missing <algorithm> header.

--- ruletable_algo.cpp
+++ ruletable_algo.cpp
@@ -32,6 +32,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
    #define strnicmp strncasecmp
 #endif
 
+#include <algorithm>
 #include <map>
 #include <sstream>
 using namespace std ;
