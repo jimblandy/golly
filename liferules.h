4874e8805ac17255ff5e50105974a3cbd5d7c69b
andykt <andykt>
2011-10-18 23:34:55 +0000

    minor doc changes

--- liferules.h
+++ liferules.h
@@ -27,7 +27,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *   algorithms, but there is no absolute requirement for this.
  *
  *   A rule lookup table is used for computing a new 2x2 grid from
- *   a provided 4x4 grid (two tables are used to emulate B0-not-S8 rules).
+ *   a provided 4x4 grid (two tables are used to emulate B0-not-Smax rules).
  *   The input is a 16-bit integer, with the most significant bit the
  *   upper left corner, bits going horizontally and then down.  The output
  *   is a 6-bit integer, with the top two bits indicating the top row of
@@ -49,10 +49,11 @@ public:
    void set_flipped() { flipped = true ; }
    int getSerial() { return serial ; }
    
-   // AKT: we need 2 tables to support B0-not-S8 rule emulation
-   char rule0[65536];      // rule table for even gens if rule has B0 but not S8,
-                           // or for all gens if rule has no B0, or it has B0 *and* S8
-   char rule1[65536];      // rule table for odd gens if rule has B0 but not S8
+   // AKT: we need 2 tables to support B0-not-Smax rule emulation
+   // where max is 8, 6 or 4 depending on the neighborhood
+   char rule0[65536];      // rule table for even gens if rule has B0 but not Smax,
+                           // or for all gens if rule has no B0, or it has B0 *and* Smax
+   char rule1[65536];      // rule table for odd gens if rule has B0 but not Smax
    bool alternate_rules;   // set by setrule; true if rule has B0 but not Smax
    
    // AKT: support for various neighborhoods
