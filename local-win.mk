26f51c39d1c6e0552d4757de58ccd156343cc6d1
rokicki <rokicki>
2012-06-08 02:22:46 +0000

    Added "excludefiles.txt", a one-line file listing "CVS", which allows us to use xcopy /exclude to make sure those directories and their files are not copied into distributions.  Most CVS implementations set the hidden bit on these CVS directories, but apparently not all, so this is a safer way to go.

--- local-win.mk
+++ local-win.mk
@@ -6,9 +6,9 @@ WX_DIR = C:\wxWidgets-2.8.7-64
 WX_RELEASE = 28
 
 # Change the next line depending on where you installed Python:
-PYTHON_INCLUDE = -I"C:\Python25-64\include"
+PYTHON_INCLUDE = -I"C:\Python27-64\include"
 
 # Simplified output from "perl -MExtUtils::Embed -e ccopts":
 PERL_INCLUDE = \
 -DHAVE_DES_FCRYPT -DNO_HASH_SEED -DUSE_SITECUSTOMIZE -DPERL_IMPLICIT_CONTEXT \
--DPERL_IMPLICIT_SYS -DUSE_PERLIO -DPERL_MSVCRT_READFIX -I"C:\Perl64\lib\CORE"
+-DPERL_IMPLICIT_SYS -DUSE_PERLIO -DPERL_MSVCRT_READFIX -I"C:\Perl514-64\lib\CORE"
