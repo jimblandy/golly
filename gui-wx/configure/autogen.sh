592304ff99f80ea886adb132cecabff9cfc6d38b
Maks Verver <maksverver@geocities.com>
2013-03-24 01:52:46 +0100

    Fix parallel source builds, srcdist target and bindist target.

--- gui-wx/configure/autogen.sh
+++ gui-wx/configure/autogen.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/sh -e
 
-DEST=gui-wx/configure/sources.am
-set -e
-rm -f "$DEST"
-echo libgolly_a_SOURCES = gollybase/*.{h,cpp} >>"$DEST"
-echo golly_SOURCES = gui-wx/*.{h,cpp} >>"$DEST"
-echo nobase_dist_pkgdata_DATA = `find {Help,Patterns,Rules,Scripts} -type f | sort` >>"$DEST"
-echo dist_doc_DATA = docs/*.html >>"$DEST"
+rm -f sources.am
+echo libgolly_a_SOURCES = ../../gollybase/*.{h,cpp} >>sources.am
+echo golly_SOURCES = ../../gui-wx/*.{h,cpp} >>sources.am
+echo 'gollydatadir = $(pkgdatadir)/Patterns/Life/' >>sources.am
+echo nobase_dist_gollydata_DATA = `find ../../{Help,Patterns,Rules,Scripts} -type f | sort` >>sources.am
+echo dist_doc_DATA = ../../docs/*.html >>sources.am
+echo EXTRA_DIST = ../../gui-wx/{makefile-{gtk,mac,win},CMakeLists.txt,Info.plist.in,*.mk,*.rc} ../../gui-wx/configure/autogen.sh ../../gui-wx/icons/* ../../gui-wx/bitmaps/* $(find ../../gui-ios -type f | sort) >>sources.am
 
-echo EXTRA_DIST = gui-wx/{makefile-{gtk,mac,win},CMakeLists.txt,Info.plist.in,*.mk,*.rc} gui-wx/configure/autogen.sh gui-wx/icons/* gui-wx/bitmaps/* $(find gui-ios -type f | sort) >>"$DEST"
-
-(cd gui-wx/configure/ && aclocal -I m4 && automake --add-missing --copy && autoconf)
+aclocal -I m4
+automake --add-missing --copy
+autoconf
