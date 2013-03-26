7cf6199ea4d6078833554a7f5079ce082b7212f2
Maks Verver <maksverver@geocities.com>
2013-03-26 23:08:41 +0100

    Only install ReadMe and License (not Build or ToDo).

--- gui-wx/configure/autogen.sh
+++ gui-wx/configure/autogen.sh
@@ -5,7 +5,6 @@ echo libgolly_a_SOURCES = ../../gollybase/*.{h,cpp} >>sources.am
 echo golly_SOURCES = ../../gui-wx/*.{h,cpp} >>sources.am
 echo 'gollydatadir = $(pkgdatadir)/Patterns/Life/' >>sources.am
 echo nobase_dist_gollydata_DATA = `find ../../{Help,Patterns,Rules,Scripts} -type f | sort` >>sources.am
-echo dist_doc_DATA = ../../docs/*.html >>sources.am
 echo EXTRA_DIST = ../../gui-wx/{makefile-{gtk,mac,win},CMakeLists.txt,Info.plist.in,*.mk,*.rc} ../../gui-wx/configure/autogen.sh ../../gui-wx/icons/* ../../gui-wx/bitmaps/* $(find ../../gui-ios -type f | sort) >>sources.am
 
 aclocal -I m4
