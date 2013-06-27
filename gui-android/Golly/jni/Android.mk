71f138aa2ab4d4c169e37f574402fbabc58c2626
Andrew Trevorrow <andykt@users.sourceforge.net>
2013-06-27 10:47:54 +1000

    moved most C++ files from gui-android/Golly/jni into new gui-common folder (the plan is to use these files in both the Android and iOS versions of Golly)

--- gui-android/Golly/jni/Android.mk
+++ gui-android/Golly/jni/Android.mk
@@ -4,26 +4,21 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := golly
 
-LOCAL_SRC_FILES := \
-    algos.cpp \
-    control.cpp \
-    file.cpp \
-    layer.cpp \
-    prefs.cpp \
-    render.cpp \
-    select.cpp \
-    status.cpp \
-    undo.cpp \
-    utils.cpp \
-    view.cpp \
-    jnicalls.cpp
+LOCAL_SRC_FILES := jnicalls.cpp
+
+# add .cpp files in gui-common
+GUI_COMMON := $(wildcard $(LOCAL_PATH)/../../../gui-common/*.cpp)
+LOCAL_SRC_FILES += $(GUI_COMMON:$(LOCAL_PATH)/%=%)
+
+# include .h files in gui-common
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../gui-common
 
 # add .cpp files in gollybase
-GOLLYBASE_FILES := $(wildcard $(LOCAL_PATH)/../../../gollybase/*.cpp)
-LOCAL_SRC_FILES += $(GOLLYBASE_FILES:$(LOCAL_PATH)/%=%)
+GOLLYBASE := $(wildcard $(LOCAL_PATH)/../../../gollybase/*.cpp)
+LOCAL_SRC_FILES += $(GOLLYBASE:$(LOCAL_PATH)/%=%)
 
-# find .h files in gollybase
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../gollybase
+# include .h files in gollybase
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../gollybase
 
 LOCAL_LDLIBS := -lGLESv1_CM -llog -lz -ljnigraphics
 
--- gui-android/Golly/jni/Application.mk
+++ gui-android/Golly/jni/Application.mk
@@ -2,9 +2,10 @@
 
 #??? APP_ABI = all
 
-# define ZLIB so Golly can read/write .gz files
-# and set -fexceptions so we can use stdexcept
-APP_CFLAGS += -DZLIB -fexceptions
+# define ANDROID_GUI used in the shared C++ code in gui-common;
+# define ZLIB so Golly can read/write .gz files;
+# set -fexceptions so we can use stdexcept
+APP_CFLAGS += -DANDROID_GUI -DZLIB -fexceptions
 
 # needed for std::string etc
 APP_STL := stlport_shared
