0d2823b660f3e5d306ee9115b64e5670fa53da96
Maks Verver <maks@verver.ch>
2016-03-25 17:16:01 +0000

    Update Autoconf scripts to build with Lua.
      - Compiling with Perl support is now disabled by default, (but still possible
        by passing --enable-perl as a configure argument).
      - Lua sources are compiled and linked into the golly executable. Sources are
        included in the source distribution.
      - We pull in the "gl" library via wx-config, which is now required.

--- gui-wx/configure/autogen.sh
+++ gui-wx/configure/autogen.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 
 rm -f sources.am
+echo nodist_liblua_a_SOURCES = ../../lua/*.{h,hpp,c} >>sources.am
 echo libgolly_a_SOURCES = ../../gollybase/*.{h,cpp} >>sources.am
 echo golly_SOURCES = ../../gui-wx/*.{h,cpp} >>sources.am
 echo 'gollydatadir = $(pkgdatadir)/Patterns/Life/' >>sources.am
 echo nobase_dist_gollydata_DATA = `find ../../{Help,Patterns,Rules,Scripts} -type f | sort` >>sources.am
 echo EXTRA_DIST = \
+	../../lua \
 	../../gui-wx/{makefile-{gtk,mac,win},CMakeLists.txt,Info.plist.in,*.mk,*.rc,configure/autogen.sh,icons,bitmaps} \
 	../../gui-common $(find ../../gui-{android,ios,web} -type f | sort) \
 	../../docs >>sources.am
