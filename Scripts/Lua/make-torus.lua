5e4b397c8cb1eb984b49ee600149c0117748f718
Andrew Trevorrow <andykt@users.sourceforge.net>
2016-04-02 14:38:33 +1100

    added make-torus.lua

---
+++ Scripts/Lua/make-torus.lua
@@ -0,0 +1,30 @@
+-- Use the current selection to create a toroidal universe.
+-- Author: Andrew Trevorrow (andrewtrevorrow.com), Apr 2016.
+
+local g = gollylib()
+
+local selrect = g.getselrect()
+if #selrect == 0 then g.exit("There is no selection.") end
+local x =  selrect[1]
+local y =  selrect[2]
+local wd = selrect[3]
+local ht = selrect[4]
+
+local selcells = g.getcells(selrect)
+if not g.empty() then
+    g.clear(0)
+    g.clear(1)
+end
+
+-- get current rule, remove any existing suffix, then add new suffix
+local rule = g.getrule()
+rule = rule:match("^(.+):") or rule
+g.setrule(string.format("%s:T%d,%d", rule, wd, ht))
+
+local newx = -math.floor(wd/2)
+local newy = -math.floor(ht/2)
+selrect[1] = newx
+selrect[2] = newy
+g.select(selrect)
+if #selcells > 0 then g.putcells(selcells, newx - x, newy - y) end
+g.fitsel()
