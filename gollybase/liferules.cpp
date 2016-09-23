3dedfbf5ba753918d38984dedabeb24a6d502bee
Brenton Bostick <bostick@gmail.com>
2016-09-23 13:03:29 -0400

    update to Android Studio 2.2

--- gollybase/liferules.cpp
+++ gollybase/liferules.cpp
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <cstdlib>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 liferules::liferules() {
    int i ;
