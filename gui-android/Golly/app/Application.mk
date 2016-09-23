16fe97bfcf274eab091bd7000ea2c992f65cfb00
Brenton Bostick <bostick@gmail.com>
2016-09-23 12:54:06 -0400

    update to Android Studio 2.2

---
+++ gui-android/Golly/app/Application.mk
@@ -0,0 +1,12 @@
+#??? APP_PLATFORM := android-17
+
+APP_ABI = armeabi-v7a armeabi
+
+# define ANDROID_GUI used in the shared C++ code in gui-common;
+# define ZLIB so Golly can read/write .gz files;
+# set -fexceptions so we can use stdexcept
+APP_CFLAGS += -DANDROID_GUI -DZLIB -fexceptions
+
+# needed for std::string etc
+APP_STL := stlport_shared
+# also need System.loadLibrary("stlport_shared") in MainActivity.java
