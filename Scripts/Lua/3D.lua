ee23721174e1a2c2b9e471a9cf9d30061ca67b00
Andrew Trevorrow <andykt@users.sourceforge.net>
2018-05-31 22:13:04 +1000

    3D.lua: commented out customcolor lines (will be enabled for 3.2b2)

--- Scripts/Lua/3D.lua
+++ Scripts/Lua/3D.lua
@@ -2361,11 +2361,12 @@ function UpdateStartButton()
     -- and also update 1st item in Control menu
     if generating then
         ssbutton.setlabel("Stop", false)
-        ssbutton.customcolor = "rgba 224 0 0 255"   -- make button background red
+        -- enable customcolor in 3.2b2!!!
+        --ssbutton.customcolor = "rgba 224 0 0 255"   -- make button background red
         mbar.setitem(3, 1, "Stop Generating")
     else
         ssbutton.setlabel("Start", false)
-        ssbutton.customcolor = nil                  -- use default background
+        --ssbutton.customcolor = nil                  -- use default background
         mbar.setitem(3, 1, "Start Generating")
     end
 end
