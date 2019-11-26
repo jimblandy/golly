abae596ea86d8bd6c9730241a7e1e84b6ee2e93c
Andrew Trevorrow <andykt@users.sourceforge.net>
2019-11-27 10:46:41 +1100

    updated version to 3.4b1

--- gui-web/main.cpp
+++ gui-web/main.cpp
@@ -1955,7 +1955,7 @@ static void DoFrame()
 
 int EMSCRIPTEN_KEEPALIVE main()
 {
-    SetMessage("This is Golly 3.3 for the web.  Copyright 2005-2019 The Golly Gang.");
+    SetMessage("This is Golly 3.4b1 for the web.  Copyright 2005-2019 The Golly Gang.");
     InitPaths();                // init tempdir, prefsfile, etc
     MAX_MAG = 5;                // maximum cell size = 32x32
     maxhashmem = 300;           // enough for caterpillar
