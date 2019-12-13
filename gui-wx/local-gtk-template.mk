6e921e6ed9909e4977d452c36b5cb76d7841f1d6
Dongook Lee <scorbiclife@gmail.com>
2019-12-13 10:36:28 +0900

    Refactor GTK Makefile
    This commit refactors the makefiles with the following changes:
    
    1. Extract program names as variables.
     This includes WX_CONFIG and PYTHON. The rationale of this change is:
    it now allows to use different versions of the same program.
    For instance, now one could use wxWidgets 2.8 and 3.1 on the same
    system (on different directories), and specify which wx-config to use.
    Same applies to Python.
    
    2. Extract user-specific directories as variables.
    This includes IRRKLANGDIR.
    
    3. Add EXTRACXXFLAGS, EXTRALDFLAGS as in local-win-template.
    This can be useful whenever we need extra flags conditionally.
    For instance, we sometimes need rpath to be relative to the build,
    so we add the appropriate flags here.
    
    4. Minor changes ("Just in case" fixes)
    - When loading Python compile and link flags, print(X) is fine when
    printing one argument (parsed as print (X)) and is futureproof.
    - Changed BITSIZE to `getconf LONG_BIT`, just in case this script gets
    more portable. (shell seems to be a GNU make extension)
    
    Unfortunately this involves "import" directives, and I am not sure how
    portable this is... (at least works in GNU make)

---
+++ gui-wx/local-gtk-template.mk
@@ -0,0 +1,25 @@
+# makefile-gtk includes local-gtk.mk, so create a copy of this file
+# and call it local-gtk.mk, then make any desired changes.
+
+# Change the next lines to specify which wx-config program to use.
+# This allows to switch to different versions of wxGTK.
+WX_CONFIG = wx-config
+
+# Lua
+LUA_DEFS = -DLUA_COMPAT_5_2
+# Uncomment the next line if building a 32-bit version of Golly:
+# LUA_DEFS = -DLUA_COMPAT_5_2 -DLUA_32BITS
+
+# Change the next line depending on where you installed Python:
+PYTHON=python2
+
+# Uncomment the next line to enable Golly to run perl scripts.
+# ENABLE_PERL = 1
+
+# Uncomment the next line to allow Golly to play sounds:
+# ENABLE_SOUND = 1
+# Change the next line to specify where you installed IrrKLang
+IRRKLANGDIR = /home/chris/irrKlang-64bit-1.6.0
+
+# Add any extra CXX flags here
+EXTRACXXFLAGS = 
