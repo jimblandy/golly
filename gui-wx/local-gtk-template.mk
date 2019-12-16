9107266472557188209a226cc1573f0623b377b2
Dongook Lee <scorbiclife@gmail.com>
2019-12-17 08:56:19 +0900

    Minor enhancements regarding flexibility

--- gui-wx/local-gtk-template.mk
+++ gui-wx/local-gtk-template.mk
@@ -26,21 +26,26 @@ IRRKLANGDIR = /home/chris/irrKlang-64bit-1.6.0
 # This one is for debian-based systems.
 # Let's just hope this is portable somehow...
 COPYLICENSECMD = \
-	for f in /usr/share/doc/*/copyright; do echo "$$f"; done \
+	find /usr/share/doc/*/copyright \
 	| grep  "$$( \
 		for f in ../.local/lib/*; do basename $$f ; done \
 		| sed -e 's|-x11||' -e 's|gmodule|glib|' -e 's|X|x|' \
 		-e 's|-|-\\{0,1\\}|' -e 's|\.so\.|-\\{0,1\\}|' \
 		-e 's|$$|/|' \
 	)" | while read -r copyright; do \
-		dest="$$(\
+		dest="$$( \
 			echo $$copyright \
 			| sed -e 's|/usr/share/doc|$(LICENSEDIR)|' \
 		)"; \
 		destdir="$$(dirname $$dest)"; \
 		echo "copying $$copyright to\t$$dest..."; \
 		mkdir -p "$$destdir" && cp "$$copyright" "$$dest"; \
-	done
+	done; \
+	# mkdir -p $(LICENSEDIR)/wxbase3.0-0 $(LICENSEDIR)/wxgtk3.0-0; \
+	cp /usr/share/doc/wxbase3.0-0/copyright $(LICENSEDIR)/wxbase3.0-0; \
+	cp /usr/share/doc/wxgtk3.0-0/copyright $(LICENSEDIR)/wxgtk3.0-0; \
+	# ^^^ Delete '#' if you installed wxwidgets from a package manager, \
+	# and edit the package versions appropriately.
 
 # Add any extra CXX and LD flags here
 # CXXFLAGS =
