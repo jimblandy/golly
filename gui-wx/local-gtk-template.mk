4cee34423161e8142530bd5944f1a597c700817c
Dongook Lee <scorbiclife@gmail.com>
2019-12-18 10:36:46 +0900

    Revert License packaging; put them in Licenses.html instead.

--- gui-wx/local-gtk-template.mk
+++ gui-wx/local-gtk-template.mk
@@ -21,32 +21,6 @@ PYTHON = python2
 # Change the next line to specify where you installed IrrKLang
 IRRKLANGDIR = /home/chris/irrKlang-64bit-1.6.0
 
-# The following is used to deal with different license paths and
-# filenames in different distributions.
-# This one is for debian-based systems.
-# Let's just hope this is portable somehow...
-COPYLICENSECMD = \
-	find /usr/share/doc/*/copyright \
-	| grep  "$$( \
-		for f in ../.local/lib/*; do basename $$f ; done \
-		| sed -e 's|-x11||' -e 's|gmodule|glib|' -e 's|X|x|' \
-		-e 's|-|-\\{0,1\\}|' -e 's|\.so\.|-\\{0,1\\}|' \
-		-e 's|$$|/|' \
-	)" | while read -r copyright; do \
-		dest="$$( \
-			echo $$copyright \
-			| sed -e 's|/usr/share/doc|$(LICENSEDIR)|' \
-		)"; \
-		destdir="$$(dirname $$dest)"; \
-		echo "copying $$copyright to\t$$dest..."; \
-		mkdir -p "$$destdir" && cp "$$copyright" "$$dest"; \
-	done; \
-	# mkdir -p $(LICENSEDIR)/wxbase3.0-0 $(LICENSEDIR)/wxgtk3.0-0; \
-	cp /usr/share/doc/wxbase3.0-0/copyright $(LICENSEDIR)/wxbase3.0-0; \
-	cp /usr/share/doc/wxgtk3.0-0/copyright $(LICENSEDIR)/wxgtk3.0-0; \
-	# ^^^ Delete '#' if you installed wxwidgets from a package manager, \
-	# and edit the package versions appropriately.
-
 # Add any extra CXX and LD flags here
 # CXXFLAGS =
 # LDFLAGS =
