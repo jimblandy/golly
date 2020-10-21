7a2995ff569dc83123376058b325b2539623b6ea
dvgrn <david.m.greene@gmail.com>
2020-10-20 20:18:02 -0500

    Improve toHistory.lua logic

--- Scripts/Lua/toHistory.lua
+++ Scripts/Lua/toHistory.lua
@@ -4,6 +4,7 @@
 -- Creates special rule and runs it for one generation, then switches to Life 
 -- Replace 2k + 1-> 1 and 2k -> 0
 -- Preserves step and generation count
+-- v1.1: skip the SuperToHistory conversion unless the current name has a "Super" suffix
 
 local g = golly()
 
@@ -63,15 +64,15 @@ end
 -- No effect if the current rule ends with "History"
 if algo == "Super" and baserule:sub(-7) == "History" then g.exit("The current rule is already a [Rule]History rule.") end
 
--- If rulestring contains "Super" suffix, remove it and continue
-if algo == "Super" and baserule:sub(-5) == "Super" then baserule = baserule:sub(1,#baserule-5) end
-
 step = g.getstep()
-
-CreateRule()
-g.setrule("SuperToHistory")
-g.run(1)
-
+-- If rulestring contains "Super" suffix, remove it and continue
+if algo == "Super" and baserule:sub(-5) == "Super" then
+    baserule = baserule:sub(1,#baserule-5)
+    CreateRule()
+    g.setrule("SuperToHistory")
+    g.run(1)
+    g.setgen("-1")
+end
 -- attempt to set the new History rule to see if it is valid
 local status, err = pcall(tryrule)
 if err then
@@ -80,6 +81,4 @@ if err then
     g.exit("Press Z to revert to the original rule.")
 end
 
-g.setalgo("Super")
 g.setstep(step)
-g.setgen("-1")
