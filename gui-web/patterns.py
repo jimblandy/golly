22e2ba70f36326406b1af2ef94ade39c6c7f8294
Andrew Trevorrow <andrew@trevorrow.com>
2020-10-23 11:10:08 +1100

    updated patterns in gui-web

--- gui-web/patterns.py
+++ gui-web/patterns.py
@@ -4,11 +4,14 @@ import os
 from os.path import join, isfile
 
 def walkdir(dir):
-    for item in os.listdir(dir):
+    for item in sorted(os.listdir(dir)):
         fullpath = join(dir, item)
         if isfile(fullpath):
             if item.startswith("."):
                 # ignore hidden files (like .DS_Store on Mac)
+                pass
+            elif item.endswith(".lua") or item.endswith(".zip"):
+                # ignore Lua scripts and zip files
                 pass
             else:
                 print "addfile(\"" + fullpath[2:] + "\");"
